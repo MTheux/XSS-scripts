51c4ec2930eb1bba7702211034d6aa123d4f0def
Amadeu Nina <87955204+MTheux@users.noreply.github.com>
2023-03-10 14:38:53 -0300

    Add files via upload
    LAB de teste de xss so executar no visual code

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site de Teste</title>
+</head>
+<body>
+    <center>
+        <form action="/index.php" method="get">
+            <label for="pesquisar">
+                Pesquisar
+            </label>
+            <input type="text" name="pesquisa" >
+            <input type="submit" value="Pesquisar" >
+        </form>
+    </center>
+</body>
+</html>
+
+
+
